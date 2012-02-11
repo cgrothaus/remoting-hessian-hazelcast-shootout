1ebc2f94c116b88314401d4b11b5b18884a289aa
Christoph Grothaus <c.grothaus@lrworld.com>
2012-02-11 21:47:17 +0100

    Methodensignatur geändert: return DataClass, um auf für Returnwert Netzwerklast zu erzeugen.

--- remoting-hessian-hazelcast-shootout-api/src/main/java/com/lr/playground/remotingshootout/hessian_hazelcast_shootout_api/HashingService.java
+++ remoting-hessian-hazelcast-shootout-api/src/main/java/com/lr/playground/remotingshootout/hessian_hazelcast_shootout_api/HashingService.java
@@ -3,6 +3,6 @@ package com.lr.playground.remotingshootout.hessian_hazelcast_shootout_api;
 public interface HashingService
 {
 
-	public int hash(DataClass data) throws HashingServiceBusinessException;
+	public DataClass hash(DataClass data) throws HashingServiceBusinessException;
 	
 }
--- remoting-hessian-hazelcast-shootout-impl/src/main/java/com/lr/playground/remotingshootout/hessian_hazelcast_shootout_impl/HashingServiceImpl.java
+++ remoting-hessian-hazelcast-shootout-impl/src/main/java/com/lr/playground/remotingshootout/hessian_hazelcast_shootout_impl/HashingServiceImpl.java
@@ -5,18 +5,19 @@ import com.lr.playground.remotingshootout.hessian_hazelcast_shootout_api.*;
 public class HashingServiceImpl implements HashingService
 {
 
-	public int hash(DataClass data) throws HashingServiceBusinessException
+	public DataClass hash(DataClass data) throws HashingServiceBusinessException
 	{
 		if ("exception".equals(data.getString1())) {
 			throw new HashingServiceBusinessException("I was told to throw up");
 		}
 		
-		int hashCode = data.getString1().hashCode();
-		hashCode = 37 * hashCode + data.getString2().hashCode();
-		hashCode = 37 * hashCode + data.getString3().hashCode();
-		hashCode = 37 * hashCode + data.getString4().hashCode();
-		hashCode = 37 * hashCode + data.getString5().hashCode();
-		return data.getString1().hashCode();
+		int hashCode = data.hashCode();
+		hashCode = hashCode * 37;
+		
+		DataClass result = new DataClass(data);
+		result.setCalculatedHashcode(hashCode);
+		
+		return result;
 	}
 
 }
